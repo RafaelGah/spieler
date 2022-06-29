2679e2dfe9aa16e8dc9ef7744c4793818b84e888
RafaelGahler <rafael.gahler@stud.kbw.ch>
2022-06-29 14:25:12 +0200

    187

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <BrowserRouter>
             <nav>
                 <Link to="/" className="navlink">Players</Link>
-                <Link to="/addPlayer" className="navlink">ad Player</Link>
+                <Link to="/addPlayer" className="navlink">add Player</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Players/>}/>
@@ -46,47 +46,64 @@ function Players() {
 }
 
 function AddPlayer() {
-    const [player, setPlayer] = useState({});
-    const [name, setName] = useState("");
-    const [surname, setSurname] = useState("");
-    const [number, setNumber] = useState(0);
-    const [birthdate, setBirthdate] = useState(Date);
-    const [nationality, setNationality] = useState("");
-    const [height, setHeight] = useState(0);
-    const [worth, setWorth] = useState(0);
-    const [club, setClub] = useState("");
-    const [url, setUrl] = useState("");
-    const addPlayer = ()=> {
+    const [player, setPlayer] = useState({
+        name: "",
+        surname: "",
+        number: 0,
+        birthdate: Date,
+        nationality: "",
+        height: 0,
+        worth: 0,
+        club: "",
+        url: ""
+    });
+    const [name, setName] = useState("name");
+    const [surname, setSurname] = useState("surname");
+    const [number, setNumber] = useState(15);
+    const [birthdate, setBirthdate] = useState("2002-12-12");
+    const [nationality, setNationality] = useState("switzerland");
+    const [height, setHeight] = useState(187);
+    const [worth, setWorth] = useState(187000000);
+    const [club, setClub] = useState("fc liverpool");
+    const [url, setUrl] = useState("https://yt3.ggpht.com/G-xQ-34A-GcfZm9ByvMnFEf1TjOatTKrJ3g0XaL1kXqCbin-7hTXhQBDe3VYtcAhx59iKG9C5jA=s900-c-k-c0x00ffffff-no-rj");
+    useEffect(() => {
+        console.log(JSON.stringify(player))
         fetch("http://localhost:8080/player", {
-            method:"POST", body: player
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(player)
         })
             .then((response) => response.json())
             .then((data) => console.log())
-    }
+    }, [player])
     return (
         <>
             <h1>Spieler hinzufügen</h1>
             <p>Name</p>
-            <input type={"text"} onChange={(e)=> setName(e.target.value)}/>
+            <input type={"text"} value={name} onChange={(e) => setName(e.target.value)}/>
             <p>Vorname</p>
-            <input type={"text"} onChange={(e)=> setSurname(e.target.value)}/>
+            <input type={"text"} value={surname} onChange={(e) => setSurname(e.target.value)}/>
             <p>Nummer</p>
-            <input type={"number"} onChange={(e)=> setNumber(e.target.value)}/>
+            <input type={"number"} value={number} onChange={(e) => setNumber(e.target.value)}/>
             <p>Geburtsdatum</p>
-            <input type={"date"} onChange={(e)=> setBirthdate(e.target.value)}/>
+            <input type={"date"} value={birthdate} onChange={(e) => setBirthdate(e.target.value)}/>
             <p>Nationalität</p>
-            <input type={"text"} onChange={(e)=> setNationality(e.target.value)}/>
+            <input type={"text"} value={nationality} onChange={(e) => setNationality(e.target.value)}/>
             <p>Grösse</p>
-            <input type={"number"} onChange={(e)=> setHeight(e.target.value)}/>
+            <input type={"number"} value={height} onChange={(e) => setHeight(e.target.value)}/>
             <p>Wert</p>
-            <input type={"number"} onChange={(e)=> setWorth(e.target.value)}/>
+            <input type={"number"} value={worth} onChange={(e) => setWorth(e.target.value)}/>
             <p>Club</p>
-            <input type={"text"} onChange={(e)=> setClub(e.target.value)}/>
-            <p>Club</p>
-            <input type={"text"} onChange={(e)=> setClub(e.target.value)}/>
+            <input type={"text"} value={club} onChange={(e) => setClub(e.target.value)}/>
             <p>Bildadresse</p>
-            <input type={"text"} onChange={(e)=> setUrl(e.target.value)}/>
-            <button onClick={setPlayer}></button>
+            <input type={"text"} value={url} onChange={(e) => setUrl(e.target.value)}/>
+            <button
+                onClick={()=>
+                setPlayer({name, surname, number, birthdate, nationality, height, worth, club, url})
+                }>Add Player</button>
         </>
     );
 }
