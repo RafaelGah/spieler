f0cb94cd808b2ada2aa4eb82b69c224dd1ad420f
RafaelGahler <rafael.gahler@stud.kbw.ch>
2022-07-06 09:10:24 +0200

    187

--- src/App.js
+++ src/App.js
@@ -20,12 +20,26 @@ function App() {
 }
 
 function Players() {
-    const [players, setPlayers] = useState([]);
-    useEffect(() => {
+    function loadPlayers() {
         fetch("http://localhost:8080/player")
             .then((response) => response.json())
             .then((data) => setPlayers(data))
+    }
+
+    const [players, setPlayers] = useState([]);
+    const [url, setUrl] = useState("")
+    useEffect(() => {
+        loadPlayers()
     }, [])
+    useEffect(() => {
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then(() => loadPlayers())
+    }, [url])
 
     return (
         <>
@@ -37,6 +51,11 @@ function Players() {
                             <img src={players.url} height={200} width={300}/>
                             <h3>{players.name + " " + players.surname}</h3>
                             <p>{"Nummer:" + " " + players.number}</p>
+                            <button onClick={UpdatePlayer()}>Update Player</button>
+                            <button onClick={() => {
+                                setUrl("http://localhost:8080/player/" + players.id)
+                            }}>set player to DELETE
+                            </button>
                         </div>
                     </li>
                 )}
@@ -101,11 +120,81 @@ function AddPlayer() {
             <p>Bildadresse</p>
             <input type={"text"} value={url} onChange={(e) => setUrl(e.target.value)}/>
             <button
-                onClick={()=>
-                setPlayer({name, surname, number, birthdate, nationality, height, worth, club, url})
-                }>Add Player</button>
+                onClick={() =>
+                    setPlayer({name, surname, number, birthdate, nationality, height, worth, club, url})
+                }
+
+            >Add Player
+            </button>
         </>
     );
 }
+
+function UpdatePlayer() {
+    const [player, setPlayer] = useState({
+        name: "",
+        surname: "",
+        number: 0,
+        birthdate: Date,
+        nationality: "",
+        height: 0,
+        worth: 0,
+        club: "",
+        url: ""
+    });
+    const [name, setName] = useState("name");
+    const [surname, setSurname] = useState("surname");
+    const [number, setNumber] = useState(15);
+    const [birthdate, setBirthdate] = useState("2002-12-12");
+    const [nationality, setNationality] = useState("switzerland");
+    const [height, setHeight] = useState(187);
+    const [worth, setWorth] = useState(187000000);
+    const [club, setClub] = useState("fc liverpool");
+    const [url, setUrl] = useState("https://yt3.ggpht.com/G-xQ-34A-GcfZm9ByvMnFEf1TjOatTKrJ3g0XaL1kXqCbin-7hTXhQBDe3VYtcAhx59iKG9C5jA=s900-c-k-c0x00ffffff-no-rj");
+    useEffect(() => {
+        console.log(JSON.stringify(player))
+        fetch("http://localhost:8080/player", {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(player)
+        })
+            .then((response) => response.json())
+            .then((data) => console.log())
+    }, [player])
+    return (
+        <>
+            <h1>Spieler hinzufügen</h1>
+            <p>Name</p>
+            <input type={"text"} value={name} onChange={(e) => setName(e.target.value)}/>
+            <p>Vorname</p>
+            <input type={"text"} value={surname} onChange={(e) => setSurname(e.target.value)}/>
+            <p>Nummer</p>
+            <input type={"number"} value={number} onChange={(e) => setNumber(e.target.value)}/>
+            <p>Geburtsdatum</p>
+            <input type={"date"} value={birthdate} onChange={(e) => setBirthdate(e.target.value)}/>
+            <p>Nationalität</p>
+            <input type={"text"} value={nationality} onChange={(e) => setNationality(e.target.value)}/>
+            <p>Grösse</p>
+            <input type={"number"} value={height} onChange={(e) => setHeight(e.target.value)}/>
+            <p>Wert</p>
+            <input type={"number"} value={worth} onChange={(e) => setWorth(e.target.value)}/>
+            <p>Club</p>
+            <input type={"text"} value={club} onChange={(e) => setClub(e.target.value)}/>
+            <p>Bildadresse</p>
+            <input type={"text"} value={url} onChange={(e) => setUrl(e.target.value)}/>
+            <button
+                onClick={() =>
+                    setPlayer({name, surname, number, birthdate, nationality, height, worth, club, url})
+                }
+
+            >Add Player
+            </button>
+        </>
+    );
+}
+
 
 export default App;
